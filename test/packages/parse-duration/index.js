76ae7717c32e6d2ed393a2a1a598cd5cfa7e3106
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-06-14 12:09:20 +0200

    Add Unit Tests for parse-duration

---
+++ test/packages/parse-duration/index.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const parseDuration = require('../../../packages/parse-duration');
+
+describe('/packages/parse-duration', () => {
+  it('1 hour 20 minutes in milliseconds', () => {
+    const actual = 4800000;
+    const expected = parseDuration('1h 20m');
+    assert.deepEqual(actual, expected);
+  });
+
+  it('1 hour 20 minutes in seconds (with second parameter)', () => {
+    const actual = 4800;
+    const expected = parseDuration('1h 20m', 's');
+    assert.deepEqual(actual, expected);
+  });
+
+  it('1 hour 20 minutes in seconds (with "in" keyword)', () => {
+    const actual = 4800;
+    const expected = parseDuration('1h 20m in s');
+    assert.deepEqual(actual, expected);
+  });
+});
