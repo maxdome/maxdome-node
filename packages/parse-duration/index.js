7c561822ddcc149c8181d8dfe9af308456d67759
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-07-11 12:15:32 +0200

    Add support for ISO 8601 output format

--- packages/parse-duration/index.js
+++ packages/parse-duration/index.js
@@ -10,6 +10,22 @@ module.exports = (ms, format) => {
     }
     ms = parse(ms);
   }
+  if (format === 'ISO 8601') {
+    let s = parseInt(ms / 1000);
+    ms = ms % 1000;
+    let m = parseInt(s / 60);
+    s = s % 60;
+    let h = parseInt(m / 60);
+    m = m % 60;
+    let d = parseInt(h / 24);
+    h = h % 24;
+    return 'P'
+      + (d ? d + 'D' : '')
+      + 'T'
+      + (h ? h + 'H' : '')
+      + (m ? m + 'M' : '')
+      + (s || ms ? (s ? s : '0') + '.' + (ms ? ('000' + ms).slice(-3) : '0') + 'S' : '');
+  }
   if (format && !parse[format] && format.substr(-1) === 's') {
     format = format.substr(0, format.length - 1);
   }
