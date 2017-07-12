581094a283e6d5efff83cba39cc50f80e4c80214
Fabian Schneider <fabian.schneider@maxdome.de>
2017-07-12 11:33:23 +0200

    Add errorLogging to logging-middleware

--- packages/logging-middleware/README.md
+++ packages/logging-middleware/README.md
@@ -7,4 +7,16 @@ app.use(require('@maxdome/logging-middleware')({
 }));
 ```
 
-See [express-winston](https://github.com/bithavoc/express-winston) for options.
+See [express-winston](https://github.com/bithavoc/express-winston#request-logging) for options.
+
+## Error Logging
+
+```
+app.use(require('@maxdome/logging-middleware').errorLogging({
+  logging,
+  options,
+  errorHandler,
+}));
+```
+
+See [express-winston#error-logging](https://github.com/bithavoc/express-winston#error-logging) for options.
--- packages/logging-middleware/index.js
+++ packages/logging-middleware/index.js
@@ -1,13 +1,12 @@
 const expressWinston = require('express-winston');
 
-module.exports = ({ logging, options }) => () => {
-  const logger = logging('request');
 module.exports = ({ logging, options }) => {
+  const requestLogger = logging('request');
 
   return expressWinston.logger(
     Object.assign(
       {
-        winstonInstance: logger,
+        winstonInstance: requestLogger,
         meta: true,
         dynamicMeta: (req, res) => ({
           path: req.route ? req.route.path : req.path,
@@ -18,4 +17,25 @@ module.exports = ({ logging, options }) => {
       options
     )
   );
+};
+
+module.exports.errorLogging = ({ logging, options, errorHandler }) => {
+  const errorLogger = logging('error');
+
+  const errorMiddlewares = [
+    expressWinston.errorLogger(
+      Object.assign({ winstonInstance: errorLogger }, options)
+    ),
+  ];
+
+  if (process.env.NODE_ENV === 'production') {
+    errorMiddlewares.push(
+      errorHandler ||
+        ((err, req, res, next) => {
+          res.sendStatus(err.status || err.statusCode || 500);
+        })
+    );
+  }
+
+  return errorMiddlewares;
 };
