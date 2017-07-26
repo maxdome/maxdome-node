e21a242b919954bd34256daf896cbb76b89ffcea
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-07-26 13:17:17 +0200

    Add "@maxdome/handler"

---
+++ packages/handler/README.md
@@ -0,0 +1,9 @@
+# Usage
+
+```javascript
+const handler = require('@maxdome/handler');
+
+app.get('/', handler(async (req, res) => {
+  res.send();
+}));
+```
---
+++ packages/handler/index.js
@@ -0,0 +1,16 @@
+module.exports = handler => async (req, res, next) => {
+  try {
+    await handler(req, res, next);
+  } catch (e) {
+    if (!res.headersSent) {
+      const status = e.status || e.statusCode || 500;
+      if (e.toString) {
+        res.status(status).send(e.toString());
+      } else if (e.message) {
+        res.status(status).send(e.message);
+      } else {
+        res.sendStatus(status);
+      }
+    }
+  }
+};
