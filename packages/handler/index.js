be3506daf2ea6dc4e63413b026fff4abc9b625f9
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-07-26 13:23:39 +0200

    Fix toString

--- packages/handler/index.js
+++ packages/handler/index.js
@@ -4,9 +4,7 @@ module.exports = handler => async (req, res, next) => {
   } catch (e) {
     if (!res.headersSent) {
       const status = e.status || e.statusCode || 500;
-      if (e.toString) {
-        res.status(status).send(e.toString());
-      } else if (e.message) {
+      if (e.message) {
         res.status(status).send(e.message);
       } else {
         res.sendStatus(status);
