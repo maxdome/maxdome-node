5b6a70e85e820e61d909bb130798504efcdd0713
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-08-10 11:48:16 +0200

    Add support for redis clusters

--- packages/redis/README.md
+++ packages/redis/README.md
@@ -1,7 +1,9 @@
 # Usage
 
 ```javascript
-const redisClient = require('@maxdome/redis')(process.env.REDIS_URL);
+const redisClient = require('@maxdome/redis')({
+  redisUrl: process.env.REDIS_URL,
+});
 
 await redisClient.setJSON('key', 'value');
 const value = await redisClient.getJSON('key');
--- packages/redis/index.js
+++ packages/redis/index.js
@@ -1,7 +1,21 @@
 const redis = require('redis');
+const RedisCluster = require('redis-clustr');
 
-module.exports = url => {
-  const redisClient = redis.createClient(url);
+module.exports = ({ redisUrl, redisClusterUrl }) => {
+  let redisClient;
+  if (redisClusterUrl) {
+    const [host, port] = redisClusterUrl.split(':');
+    redisClient = new RedisCluster({
+      servers: [
+        {
+          host,
+          port
+        }
+      ]
+    });
+  } else {
+    redisClient = redis.createClient(redisUrl);
+  }
 
   redisClient.setJSON = (key, value, callback) => {
     value = JSON.stringify(value);
