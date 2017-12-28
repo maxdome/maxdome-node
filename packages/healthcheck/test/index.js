f237fda3a53c74d16b362775b783fd8ac78aadf4
Christian Neuhäuser <chris@type10.com>
2017-12-28 14:53:23 +0100

    Now using GitHub health checks

--- packages/healthcheck/test/index.js
+++ packages/healthcheck/test/index.js
@@ -14,36 +14,36 @@ describe('/packages/healthcheck', () => {
     it('allows advanced config', () => {
       app.use(
         '/health',
-        healthcheck({ healthChecks: { heimdall: { method: 'POST', url: 'https://heimdall.maxdome.de/info' } } })
+        healthcheck({ healthChecks: { github: { method: 'POST', url: 'https://status.github.com/api/status.json' } } })
       );
       return request(app)
         .get('/health')
         .expect(503)
         .then(res => {
-          assert.deepEqual(res.body, { heimdall: 404 });
+          assert.deepEqual(res.body, { github: 404 });
         });
     });
     it('returns status 503 if a service is erroneous', () => {
       app.use(
         '/health',
         healthcheck({
-          healthChecks: { google: 'https://google.com/ping', heimdall: 'https://heimdall.maxdome.de/info' },
+          healthChecks: { google: 'https://google.com/ping', github: 'https://status.github.com/api/status.json' },
         })
       );
       return request(app)
         .get('/health')
         .expect(503)
         .then(res => {
-          assert.deepEqual(res.body, { google: 400, heimdall: 200 });
+          assert.deepEqual(res.body, { google: 400, github: 200 });
         });
     });
     it('returns status 200 all services are ok', () => {
-      app.use('/health', healthcheck({ healthChecks: { heimdall: 'https://heimdall.maxdome.de/info' } }));
+      app.use('/health', healthcheck({ healthChecks: { github: 'https://status.github.com/api/status.json' } }));
       return request(app)
         .get('/health')
         .expect(200)
         .then(res => {
-          assert.deepEqual(res.body, { heimdall: 200 });
+          assert.deepEqual(res.body, { github: 200 });
         });
     });
   });
@@ -53,25 +53,25 @@ describe('/packages/healthcheck', () => {
       app.use(
         '/health',
         healthcheck({
-          healthChecks: { heimdall: { method: 'POST', url: 'https://heimdall-ext-test2.maxdome.de/health/app' } },
+          healthChecks: { github: { method: 'POST', url: 'https://status.github.com/api/status.json' } },
         })
       );
       return request(app)
         .get('/health/details')
         .expect(503)
         .then(res => {
-          assert.equal(res.body.heimdall.method, 'POST');
-          assert.equal(res.body.heimdall.url, 'https://heimdall-ext-test2.maxdome.de/health/app');
-          assert.equal(res.body.heimdall.status, 404);
-          assert.equal(typeof res.body.heimdall.request.headers, 'object');
-          assert.equal(typeof res.body.heimdall.response.headers, 'object');
+          assert.equal(res.body.github.method, 'POST');
+          assert.equal(res.body.github.url, 'https://status.github.com/api/status.json');
+          assert.equal(res.body.github.status, 404);
+          assert.equal(typeof res.body.github.request.headers, 'object');
+          assert.equal(typeof res.body.github.response.headers, 'object');
         });
     });
     it('returns status 503 if a service is erroneous', () => {
       app.use(
         '/health',
         healthcheck({
-          healthChecks: { google: 'https://google.com/ping', heimdall: 'https://heimdall.maxdome.de/info' },
+          healthChecks: { google: 'https://google.com/ping', github: 'https://status.github.com/api/status.json' },
         })
       );
       return request(app)
@@ -83,28 +83,28 @@ describe('/packages/healthcheck', () => {
           assert.equal(res.body.google.status, 400);
           assert.equal(typeof res.body.google.request.headers, 'object');
           assert.equal(typeof res.body.google.response.headers, 'object');
-          assert.equal(res.body.heimdall.method, 'GET');
-          assert.equal(res.body.heimdall.url, 'https://heimdall.maxdome.de/info');
-          assert.equal(res.body.heimdall.status, 200);
-          assert.equal(typeof res.body.heimdall.request.headers, 'object');
-          assert.equal(typeof res.body.heimdall.response.headers, 'object');
+          assert.equal(res.body.github.method, 'GET');
+          assert.equal(res.body.github.url, 'https://status.github.com/api/status.json');
+          assert.equal(res.body.github.status, 200);
+          assert.equal(typeof res.body.github.request.headers, 'object');
+          assert.equal(typeof res.body.github.response.headers, 'object');
         });
     });
     it('returns status 200 all services are ok', () => {
       app.use(
         '/health',
-        healthcheck({ healthChecks: { heimdall: 'https://heimdall-ext-test2.maxdome.de/health/app' } })
+        healthcheck({ healthChecks: { github: 'https://status.github.com/api/status.json' } })
       );
       return request(app)
         .get('/health/details')
         .expect(200)
         .then(res => {
-          assert.equal(res.body.heimdall.method, 'GET');
-          assert.equal(res.body.heimdall.url, 'https://heimdall-ext-test2.maxdome.de/health/app');
-          assert.equal(res.body.heimdall.status, 200);
-          assert.equal(typeof res.body.heimdall.request.headers, 'object');
-          assert.equal(typeof res.body.heimdall.response.headers, 'object');
-          assert.equal(typeof res.body.heimdall.response.body, 'object');
+          assert.equal(res.body.github.method, 'GET');
+          assert.equal(res.body.github.url, 'https://status.github.com/api/status.json');
+          assert.equal(res.body.github.status, 200);
+          assert.equal(typeof res.body.github.request.headers, 'object');
+          assert.equal(typeof res.body.github.response.headers, 'object');
+          assert.equal(typeof res.body.github.response.body, 'object');
         });
     });
   });
