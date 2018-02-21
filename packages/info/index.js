eab5f9ad205fcbcb2c0be1c39c58bf4ccb3d7beb
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-02-21 12:23:22 +0100

    Remove default output of all env vars

--- packages/info/index.js
+++ packages/info/index.js
@@ -7,11 +7,13 @@ const path = require('path');
 
 module.exports = () => {
   const router = new express.Router();
+
   const version = require('@maxdome/version')();
   router.get('/version', (req, res) => {
     res.send(version);
   });
-  let env = process.env;
+
+  let env = {};
   try {
     const keys = fs
       .readFileSync(path.join(process.cwd(), '.env.example'), 'utf-8')
@@ -23,14 +25,16 @@ module.exports = () => {
         }
       })
       .filter(key => key);
-    env = _.pick(env, keys);
+    env = _.pick(process.env, keys);
   } catch (e) {
     if (e.code !== 'ENOENT') {
       throw e;
     }
   }
+  
   router.get('/env', (req, res) => {
     res.send(env);
   });
+
   return router;
 };
