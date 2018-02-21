bb6ca573d0394cfbdee4fb68a8b1cdbc1ef55af5
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-02-21 11:16:23 +0100

    Fix format

--- packages/version/index.js
+++ packages/version/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 let version;
 module.exports = () => {
   if (!version) {
-    version = `v${require(path.join(process.cwd(), 'package.json')).version}`;
+    version = require(path.join(process.cwd(), 'package.json')).version;
     try {
       version += '-' + fs.readFileSync(path.join(process.cwd(), 'REVISION'), 'utf-8').trim();
     } catch (e) {
