f7d61b600d2881a0c237e9dd8a465d2aca2c346e
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-02-21 14:20:58 +0100

    Remove the JSON parse for the health response

--- packages/health-got/index.js
+++ packages/health-got/index.js
@@ -5,7 +5,6 @@ module.exports = data => {
     data = { url: data };
   }
   data.method = data.method || 'get';
-  data.options = Object.assign({ json: true }, data.options);
   return {
     check: () => got[data.method](data.url, data.options),
     data: { url: data.url },
