ed2303d7dc91de8d0d8d18acbaa98705f0a1112a
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-03-06 10:33:45 +0100

    Refactor the app startup to be async

--- skeletons/web/src/server.js
+++ skeletons/web/src/server.js
@@ -1,25 +1,32 @@
-require('@maxdome/env');
+module.exports = async () => {
+  require('@maxdome/env');
 
-const logging = require('@maxdome/logging')({
-  level: process.env.LOG_LEVEL,
-});
-require('@maxdome/exception-handling')({ logging });
-const appLogger = logging('app');
-appLogger.info('initializing server');
-const app = require('@maxdome/express')();
-app.use(require('@maxdome/logging-middleware')({ logging }));
-
-app.use('/docs', require('@maxdome/swagger')({ config: 'docs/swagger.yml' }));
-app.get('/health', require('@maxdome/health').controller());
-app.use('/info', require('@maxdome/info')());
-
-app.use('/api', require('./api')());
-
-app.use(require('@maxdome/logging-middleware').errorLogging({ logging }));
-if (!module.parent) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    appLogger.info(`Server listening on ${port}`);
+  const logging = require('@maxdome/logging')({
+    level: process.env.LOG_LEVEL,
   });
+  require('@maxdome/exception-handling')({ logging });
+  const appLogger = logging('app');
+  appLogger.info('initializing server');
+  const app = require('@maxdome/express')();
+  app.use(require('@maxdome/logging-middleware')({ logging }));
+
+  app.use('/docs', require('@maxdome/swagger')({ config: 'docs/swagger.yml' }));
+  app.get('/health', require('@maxdome/health').controller());
+  app.use('/info', require('@maxdome/info')());
+
+  app.use('/api', require('./api')());
+
+  app.use(require('@maxdome/logging-middleware').errorLogging({ logging }));
+
+  return { app, logging };
+};
+
+if (!module.parent) {
+  (async () => {
+    const { app, logging } = await module.exports();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      logging('app').info(`Server listening on ${port}`);
+    });
+  })();
 }
-module.exports = app;
