b3400150018e60e413f2ac7c74574a4ff8cfd6ff
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-05-18 10:03:09 +0200

    Allow to define also a logger

--- packages/request-maxdome/src/getRequestBuilder.js
+++ packages/request-maxdome/src/getRequestBuilder.js
@@ -4,8 +4,25 @@ const MaxdomeOptions = require('./MaxdomeOptions');
 const RequestBuilder = require('@maxdome/request').RequestBuilder;
 const TipOfTheDaysOptions = require('./TipOfTheDaysOptions');
 
-module.exports = ({ assetOptions: assetOptions = {}, log, maxdomeOptions: maxdomeOptions = {} } = {}) =>
+module.exports = ({
+  assetOptions: assetOptions = {
+    hostnames: (hostnames = {
+      package: process.env.ASSET_HOSTNAME_PACKAGE || 'www.maxdome.de',
+      store: process.env.ASSET_HOSTNAME_STORE || 'store.maxdome.de',
+    }),
+    protocol: (protocol = process.env.ASSET_PROTOCOL),
+  },
+  log,
+  logger,
+  maxdomeOptions: maxdomeOptions = {
+    apikey: process.env.MAXDOME_APIKEY,
+    appid: process.env.MAXDOME_APPID,
+    hostname: process.env.MAXDOME_HOSTNAME,
+    protocol: process.env.MAXDOME_PROTOCOL,
+    url: process.env.MAXDOME_URL,
+  },
+} = {}) =>
   new RequestBuilder(new MaxdomeOptions(maxdomeOptions))
-    .addErrorLogger(new MaxdomeErrorLogger(log))
+    .addErrorLogger(new MaxdomeErrorLogger({ log, logger }))
     .setOptions('assets', new AssetsOptions(assetOptions))
     .setOptions('tipOfTheDays', new TipOfTheDaysOptions());
