752053e53a0247b5bca3c5c3a97c6b62bb4ef44f
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-05-18 10:03:17 +0200

    Allow to define also a logger

--- packages/request-maxdome/src/MaxdomeErrorLogger.js
+++ packages/request-maxdome/src/MaxdomeErrorLogger.js
@@ -1,13 +1,17 @@
 class MaxdomeErrorLogger {
-  constructor(log = console.log) {
-    this.log = log;
+  constructor({ log, logger: logger = console } = {}) {
+    if (log) {
+      this.logger = { log };
+    } else {
+      this.logger = logger;
+    }
   }
 
   onError(e) {
     if (e.statusCode === 403) {
       const matches = /Reference #([a-zA-Z0-9.]*)/.exec(e.message);
       if (matches) {
-        this.log(`KONA error: ${matches[0]}`);
+        this.logger.log(`KONA error: ${matches[0]}`);
       }
     }
   }
